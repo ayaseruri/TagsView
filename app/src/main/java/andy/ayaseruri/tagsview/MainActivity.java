646db0eaaff5e1bd5b91c29ad94b96736b5780fb
ayaseruri <ayaseruri@foxmail.com>
2015-08-13 22:35:59 +0800

    init

---
+++ app/src/main/java/andy/ayaseruri/tagsview/MainActivity.java
@@ -0,0 +1,67 @@
+package andy.ayaseruri.tagsview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import andy.ayaseruri.lib.TagsView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TagsView tagsView = (TagsView)findViewById(R.id.tags_view);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tagsView.init(new ArrayList<String>() {{
+                    add("tags");
+                    add("tags_tow");
+                    add("tags_hello");
+                    add("tags_withtagsview");
+                    add("t");
+                    add("ta");
+                    add("tags");
+                    add("tags_withtagsview");
+                    add("tags_withtagsview");
+                    ;
+                }}
+                        , new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "tags clicked", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ lib/src/main/java/andy/ayaseruri/lib/TagsView.java
@@ -0,0 +1,117 @@
+package andy.ayaseruri.lib;
+
+/**
+ * Created by ayaseruri on 15/8/13.
+ */
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ayaseruri on 15/8/2.
+ */
+public class TagsView extends LinearLayout {
+
+    private int textPaddingLeft, textPaddingRight, textPaddingTop, textPaddingBottom, tagsSpace, textSize;
+    private int leftMargin, rightMargin, maxLength, textColor;
+    private int tagsBg, maxLines;
+
+    private Context mContext;
+
+    public TagsView(Context context) {
+        super(context);
+    }
+
+    public TagsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mContext = context;
+
+        this.setOrientation(VERTICAL);
+        this.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TagsView);
+        textPaddingLeft = a.getDimensionPixelSize(R.styleable.TagsView_textPaddingLeft, 0);
+        textPaddingRight = a.getDimensionPixelSize(R.styleable.TagsView_textPaddingRight, 0);
+        textPaddingTop = a.getDimensionPixelSize(R.styleable.TagsView_textPaddingTop, 0);
+        textPaddingBottom = a.getDimensionPixelSize(R.styleable.TagsView_textPaddingBottom, 0);
+        textSize = a.getDimensionPixelSize(R.styleable.TagsView_textSize, 0);
+        textColor = a.getColor(R.styleable.TagsView_textColor, getResources().getColor(android.R.color.black));
+        tagsSpace = a.getDimensionPixelSize(R.styleable.TagsView_tagsSpace, 0);
+        tagsBg = a.getResourceId(R.styleable.TagsView_tagsBg, R.drawable.tags_bg);
+        leftMargin = a.getDimensionPixelSize(R.styleable.TagsView_leftMargin, 0);
+        rightMargin = a.getDimensionPixelSize(R.styleable.TagsView_rightMargin, 0);
+        maxLines = a.getInt(R.styleable.TagsView_maxlines, 1);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        wm.getDefaultDisplay().getMetrics(dm);
+        maxLength = a.getDimensionPixelSize(R.styleable.TagsView_maxLength, dm.widthPixels);
+    }
+
+    public void init(ArrayList<String> list, OnClickListener onClickListener){
+        this.removeAllViews();
+        float rowLength = leftMargin + rightMargin;
+        LinearLayout row = creatLinearLayout();
+        int line = 0;
+
+        for(String tagText : list){
+            TextView tag = new TextView(mContext);
+            tag.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+            tag.setTextColor(textColor);
+            tag.setMaxLines(1);
+            tag.setEllipsize(TextUtils.TruncateAt.END);
+            tag.setMinimumWidth(0);
+            tag.setMinimumHeight(0);
+            tag.setIncludeFontPadding(false);
+            tag.setPadding(textPaddingLeft, textPaddingTop, textPaddingRight, textPaddingBottom);
+            tag.setText(tagText);
+            tag.setBackgroundResource(tagsBg);
+            if(null != onClickListener){
+                tag.setClickable(true);
+                tag.setOnClickListener(onClickListener);
+            }
+
+            LayoutParams btnParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            btnParams.setMargins(0, 0, tagsSpace, 0);
+            tag.setLayoutParams(btnParams);
+
+            rowLength = rowLength + tag.getPaint().measureText(tagText) + tagsSpace;
+            if(rowLength < maxLength){
+                row.addView(tag);
+            }else {
+                if(line < maxLines){
+                    this.addView(row);
+                    line++;
+                    rowLength = leftMargin + rightMargin + tag.getPaint().measureText(tagText) + tagsSpace;
+                    row = creatLinearLayout();
+                    row.addView(tag);
+                }else {
+                   return;
+                }
+            }
+        }
+        if(0 != row.getChildCount() && line < maxLines){
+            this.addView(row);
+        }
+    }
+
+    private LinearLayout creatLinearLayout(){
+        LinearLayout linearLayout = new LinearLayout(mContext);
+        LayoutParams layoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftMargin = leftMargin;
+        layoutParams.rightMargin = rightMargin;
+        linearLayout.setLayoutParams(layoutParams);
+        return linearLayout;
+    }
+}
+
